5b4e259699db8e5711fd7fb4e88e3c62a035c979
Joel Heath <joelheath@outlook.com>
2026-09-20 20:13:12 +0100

    Migration complete, removing migration scripts.

--- migrate_whitelist.sql
+++
@@ -1,5 +0,0 @@
--- Migration to create whitelisted_emails table
-CREATE TABLE IF NOT EXISTS whitelisted_emails (
-    email       TEXT PRIMARY KEY NOT NULL COLLATE NOCASE,
-    created_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
